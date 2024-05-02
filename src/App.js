646663984c35496522c885d1b40b676a03683ea4
anzhdaudysh <anzhdaudish@icloud.com>
2024-05-02 22:16:38 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 // import './App.css';
-
+// scenario
 import React from 'react';
 import { useState, useEffect, useRef } from 'react';
 import { createRoot } from 'react-dom/client';
